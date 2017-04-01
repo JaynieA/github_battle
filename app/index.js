22a5736bea3439b9b256155f705859127aa0bb40
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-04-01 15:09:39 -0500

    add and display avatar components

--- app/index.js
+++ app/index.js
@@ -1,27 +1,45 @@
+const USER_DATA = {
+  name: 'Jaynie Anderson',
+  username: 'JaynieA',
+  image: 'https://avatars2.githubusercontent.com/u/22354326?v=3&s=460'
+};
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-//Create a component
-const HelloWorld = React.createClass({
-  //render returns what the UI of this component looks like
-  render: function() {
-    return (
-      //Babel will transpile this JSX --> JS
-      <div> Hello ReactJS Program </div>
-    )
+class ProfilePic extends React.Component {
+  render() {
+    return <img src={ this.props.imageUrl } style={{ height: 100, width: 100 }} />
   }
-});
+}
 
-//Doing the above using ES6 classes:
-// class HelloWorld extends React.Component {
-//   render() {
-//     return <div> Hello World </div>
-//   }
-// }
+class ProfileLink extends React.Component {
+  render() {
+    return <div>
+             <a href={ 'https://www.github.com/' + this.props.username }>{ this.props.username }</a>
+           </div>
+  }
+}
+
+class ProfileName extends React.Component {
+  render() {
+    return <div>{ this.props.name }</div>
+  }
+}
+
+class Avatar extends React.Component {
+  render() {
+    return <div>
+              <ProfilePic imageUrl={ this.props.user.image } />
+              <ProfileName name={ this.props.user.name }/>
+              <ProfileLink username={ this.props.user.username }/>
+           </div>
+  }
+}
 
 ReactDOM.render(
   //How you invoke/use the component defined above
   //Says- grab the component passed (HelloWorld), and render it to 'app'
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
