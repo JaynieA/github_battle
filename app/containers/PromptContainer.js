e9d6a0ab7a1476256fd6a1a4c7b9f4a23f69cb6b
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-04-02 21:12:41 -0500

    refactor to container and presentation, functional components

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import Prompt from '../components/Prompt';
 
+//Handles the logic for UI creation in Prompt component
 class PromptContainer extends React.Component {
   //constructor method necessary when using es6 class syntax
   constructor(props: Object, context: Object) {
     super(props, context);
     this.state = {
       username: '',
-    };
+    }; // end state
     /* since there is no autobinding when using es6 classes,
       we need to bind the event handlers */
     this.handleUpdateUser = this.handleUpdateUser.bind(this);
     this.handleSubmitUser = this.handleSubmitUser.bind(this);
-  }
+  } // end constructor
   handleUpdateUser(e) {
     this.setState({
       username: e.target.value
-    });
-  }
+    }); // end setState
+  } // end handleUpdateUser
   handleSubmitUser(e) {
     //prevent page from reloading
     e.preventDefault();
@@ -25,8 +26,7 @@ class PromptContainer extends React.Component {
     let username = this.state.username;
     this.setState({
       username: ''
-    });
-
+    }); // end setState
     //If on playerOne route, when click continue - go to /playerTwo
     // if on playerTwo route, when click continue - go to /battle
     if (this.props.routeParams.playerOne) {
@@ -37,13 +37,13 @@ class PromptContainer extends React.Component {
         query: {
           playerOne: this.props.routeParams.playerOne,
           playerTwo: this.state.username
-        }
-      });
+        } // end query
+      }); // end push
     } else {
       // go to /playerTwo
       this.context.router.push('/playerTwo/' + this.state.username );
-    }
-  }
+    } // end else
+  } // end handleSubmitUser
   render() {
     //Render the UI of our prompt component
     return (
@@ -52,12 +52,12 @@ class PromptContainer extends React.Component {
         onUpdateUser={this.handleUpdateUser}
         header={this.props.route.header}
         username={this.state.username} />
-    )
-  }
-}
+    ) // end return
+  } // end render
+} // end PromptContainer
 
 PromptContainer.contextTypes = {
     router: React.PropTypes.object.isRequired
-};
+} // end contextTypes
 
 export default PromptContainer;
